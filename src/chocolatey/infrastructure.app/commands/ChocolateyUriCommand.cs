49e82711d180a7953eae233a2776b94aaa410294
Gary Ewan Park <gep13@gep13.co.uk>
2024-06-27 02:38:38 -0700

    Stub out new choco uri command

---
+++ src/chocolatey/infrastructure.app/commands/ChocolateyUriCommand.cs
@@ -0,0 +1,98 @@
+﻿// Copyright © 2017 - 2021 Chocolatey Software, Inc
+// Copyright © 2011 - 2017 RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using chocolatey.infrastructure.app.attributes;
+using chocolatey.infrastructure.app.configuration;
+using chocolatey.infrastructure.commandline;
+using chocolatey.infrastructure.commands;
+using System;
+using System.Collections.Generic;
+
+namespace chocolatey.infrastructure.app.commands
+{
+    [CommandFor("uri", "allows mainteance of a package via the choco:// protocol")]
+    public class ChocolateyUriCommand : ICommand
+    {
+        public void ConfigureArgumentParser(OptionSet optionSet, ChocolateyConfiguration configuration)
+        {
+            // Deliberately left blank, as there isn't anything to be done here.
+        }
+
+        public void DryRun(ChocolateyConfiguration configuration)
+        {
+            // TODO: Need to put stuff here
+        }
+
+        public void HelpMessage(ChocolateyConfiguration configuration)
+        {
+            // TODO: Need to put stuff here
+        }
+
+        public bool MayRequireAdminAccess()
+        {
+            return true;
+        }
+
+        public void ParseAdditionalArguments(IList<string> unparsedArguments, ChocolateyConfiguration configuration)
+        {
+            this.Log().Info("Parsing stuff...");
+            foreach(var unparsedArgument in unparsedArguments)
+            {
+                this.Log().Info(unparsedArgument);
+            }
+        }
+
+        public void Run(ChocolateyConfiguration config)
+        {
+            this.Log().Info("Got here...");
+        }
+
+        public void Validate(ChocolateyConfiguration configuration)
+        {
+            // Intentionally left blank
+        }
+
+#pragma warning disable IDE0022, IDE1006
+        [Obsolete("This overload is deprecated and will be removed in v3.")]
+        public virtual void configure_argument_parser(OptionSet optionSet, ChocolateyConfiguration configuration)
+            => ConfigureArgumentParser(optionSet, configuration);
+
+        [Obsolete("This overload is deprecated and will be removed in v3.")]
+        public virtual void handle_additional_argument_parsing(IList<string> unparsedArguments, ChocolateyConfiguration configuration)
+            => ParseAdditionalArguments(unparsedArguments, configuration);
+
+        [Obsolete("This overload is deprecated and will be removed in v3.")]
+        public virtual void handle_validation(ChocolateyConfiguration configuration)
+            => Validate(configuration);
+
+        [Obsolete("This overload is deprecated and will be removed in v3.")]
+        public virtual void help_message(ChocolateyConfiguration configuration)
+            => HelpMessage(configuration);
+
+        [Obsolete("This overload is deprecated and will be removed in v3.")]
+        public virtual void noop(ChocolateyConfiguration configuration)
+            => DryRun(configuration);
+
+        [Obsolete("This overload is deprecated and will be removed in v3.")]
+        public virtual void run(ChocolateyConfiguration configuration)
+            => Run(configuration);
+
+        [Obsolete("This overload is deprecated and will be removed in v3.")]
+        public virtual bool may_require_admin_access()
+            => MayRequireAdminAccess();
+#pragma warning restore IDE0022, IDE1006
+    }
+}
